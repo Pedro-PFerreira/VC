fd26bc2fca08347d59de2044d4ed0f5a901f4a7f
ricardoinaciopt <ricardo.inacio.2002@gmail.com>
2024-04-01 10:41:13 +0100

    implemented color detection
    Co-authored-by: Lourenço Gonçalves <lougon02@users.noreply.github.com>
    Co-authored-by: Pedro Ferreira <Pedro-PFerreira@users.noreply.github.com>

--- Project/pipelines/testing/testing.py
+++ Project/pipelines/testing/testing.py
@@ -10,24 +10,30 @@ with open("./json/test.json", "r") as file:
     json_test = json.load(file)
     file.close()
 
+
 def test(filename, num_bricks, num_colors):
     global brick_error_acc, color_error_acc, n_tests
 
-    for test_case in json_test["results"]:  # Access the "results" key in the JSON object
+    for test_case in json_test[
+        "results"
+    ]:  # Access the "results" key in the JSON object
         if test_case["file_name"] == filename or test_case["file_name"] in filename:
             print("\n\nTesting: " + filename)
             print("=" * 30)
-            
+
             if test_case["num_detections"] == num_bricks:
                 print(Fore.GREEN + "Number of detections test passed" + Style.RESET_ALL)
             else:
                 print(Fore.RED + "Number of detections test failed" + Style.RESET_ALL)
 
-            brick_error_acc += abs(test_case["num_detections"] - num_bricks) / test_case["num_detections"]
+            brick_error_acc += (
+                abs(test_case["num_detections"] - num_bricks)
+                / test_case["num_detections"]
+            )
 
             print(
-                f"\tExpected number of bricks: {num_bricks}\n"
-                f"\tDetected number of bricks: {test_case['num_detections']}\n"
+                f"\tExpected number of bricks: {test_case['num_detections']}\n"
+                f"\tDetected number of bricks: {num_bricks}\n"
             )
 
             if test_case["num_colors"] == num_colors:
@@ -35,15 +41,18 @@ def test(filename, num_bricks, num_colors):
             else:
                 print(Fore.RED + "Number of colors test failed" + Style.RESET_ALL)
 
-            color_error_acc += abs(test_case["num_colors"] - num_colors) / test_case["num_colors"]
+            color_error_acc += (
+                abs(test_case["num_colors"] - num_colors) / test_case["num_colors"]
+            )
 
             print(
-                f"\tExpected number of colors: {num_colors}\n"
-                f"\tDetected number of colors: {test_case['num_colors']}\n"
+                f"\tExpected number of colors: {test_case['num_colors']}\n"
+                f"\tDetected number of colors: {num_colors}\n"
             )
-    
+
     n_tests += 1
-        
+
+
 def compute_testing_score():
     print("=" * 30)
     print("Testing Summary")
